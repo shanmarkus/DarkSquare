81dd45b96c40d3cb4e5cb34917f5b9de9a945574
shanmarkus <shanmarkus@gmail.com>
2014-10-03 17:30:23 +0700

    Adding Main Content

--- application/views/main_content.php
+++ application/views/main_content.php
@@ -54,11 +54,36 @@
 </div>
 </div>
 
-</div> 
+</div> <!-- END OF BLOG CONTENT -->
 
 <!-- MAIN CONTENT -->
-<div class="col-md-6">.col-md-1</div>
+<div class="col-md-5 content-main">
+
+  <div class="row">
+    <div class="col-md-12">
+      <img src="<?php echo IMAGES_URL?>image_1.png" class="img-responsive content-image" alt="Responsive image"></img>
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="col-md-12">
+      <h4>Menata Ruang Keluarga</h4>
+      <hr class="style-one"> </hr>
+      <p class="blog-text">Masukkan penjelasan singkat disini atau beberapa kata dan kalimat pertama dari blog yang dituju.</p>
+    </div>
+  </div>
+</div>
 
 <!-- ADS CONTENT -->
 <div class="col-md-3">.col-md-1</div>
 </div>
+
+
+
+
+
+
+
+
+
+
