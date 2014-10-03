c75979e1ab56746a27ecc847f7c9debe3ae7dc64
shanmarkus <shanmarkus@gmail.com>
2014-10-03 18:23:44 +0700

    fixing view

--- application/views/main_content.php
+++ application/views/main_content.php
@@ -10,69 +10,86 @@
       </div>
     </div>
 
-    <!--Repetitive Blog Content -->
+    <div class="content blog">
+      <div class="row">
+       <div class="col-md-6">
+         <img src="<?php echo IMAGES_URL?>image_2.png" class="blog_image"></img>
+       </div>
+       <div class="col-md-6">
+         <h4 class="marg-top-0">Menata Ruang Keluarga</h4>
+         <p class="blog-text">Masukkan penjelasan singkat disini atau beberapa kata dan kalimat pertama dari blog yang dituju.</p>
+       </div>
+     </div>
+   </div> 
+
+   <div class="content blog">
     <div class="row">
      <div class="col-md-6">
        <img src="<?php echo IMAGES_URL?>image_2.png" class="blog_image"></img>
      </div>
      <div class="col-md-6">
-      <h4 class="marg-top-0">Menata Ruang Keluarga</h4>
-      <p class="blog-text">Masukkan penjelasan singkat disini atau beberapa kata dan kalimat pertama dari blog yang dituju.</p>
-    </div>
-  </div>
+       <h4 class="marg-top-0">Menata Ruang Keluarga</h4>
+       <p class="blog-text">Masukkan penjelasan singkat disini atau beberapa kata dan kalimat pertama dari blog yang dituju.</p>
+     </div>
+   </div>
+ </div> 
 
-  <!--Repetitive Blog Content -->
+ <div class="content blog">
   <div class="row">
    <div class="col-md-6">
      <img src="<?php echo IMAGES_URL?>image_2.png" class="blog_image"></img>
    </div>
    <div class="col-md-6">
-    <h4 class="marg-top-0">Menata Ruang Keluarga</h4>
-    <p class="blog-text">Masukkan penjelasan singkat disini atau beberapa kata dan kalimat pertama dari blog yang dituju.</p>
-  </div>
-</div>
-
-<!--Repetitive Blog Content -->
-<div class="row">
- <div class="col-md-6">
-   <img src="<?php echo IMAGES_URL?>image_2.png" class="blog_image"></img>
+     <h4 class="marg-top-0">Menata Ruang Keluarga</h4>
+     <p class="blog-text">Masukkan penjelasan singkat disini atau beberapa kata dan kalimat pertama dari blog yang dituju.</p>
+   </div>
  </div>
- <div class="col-md-6">
-  <h4 class="marg-top-0">Menata Ruang Keluarga</h4>
-  <p class="blog-text">Masukkan penjelasan singkat disini atau beberapa kata dan kalimat pertama dari blog yang dituju.</p>
-</div>
-</div>
+</div> 
 
-<!--Repetitive Blog Content -->
-<div class="row">
- <div class="col-md-6">
-   <img src="<?php echo IMAGES_URL?>image_2.png" class="blog_image"></img>
- </div>
- <div class="col-md-6">
-  <h4 class="marg-top-0">Menata Ruang Keluarga</h4>
-  <p class="blog-text">Masukkan penjelasan singkat disini atau beberapa kata dan kalimat pertama dari blog yang dituju.</p>
-</div>
-</div>
 
-</div> <!-- END OF BLOG CONTENT -->
+
+</div><!-- END OF BLOG CONTENT -->
 
 <!-- MAIN CONTENT -->
-<div class="col-md-5 content-main">
+<div class="col-md-6">
 
-  <div class="row">
+  <!-- Main Content -->
+  <div class="row content-main">
     <div class="col-md-12">
       <img src="<?php echo IMAGES_URL?>image_1.png" class="img-responsive content-image" alt="Responsive image"></img>
     </div>
   </div>
 
-  <div class="row">
+  <div class="row content-main">
     <div class="col-md-12">
       <h4>Menata Ruang Keluarga</h4>
       <hr class="style-one"> </hr>
       <p class="blog-text">Masukkan penjelasan singkat disini atau beberapa kata dan kalimat pertama dari blog yang dituju.</p>
     </div>
   </div>
+
+  <!-- Sub Content -->
+  <div class="sub-content">
+   <div class="row pad-top-20">
+
+    <div class="col-md-5 col-sm-offset-1 sub-content-main">
+      <h4>Menata Ruang Keluarga</h4>
+      <hr class="style-one"> </hr>
+      <p class="blog-text">Masukkan penjelasan singkat disini atau beberapa kata dan kalimat pertama dari blog yang dituju.</p>
+    </div>
+
+    <div class="col-md-5 sub-content-main">
+      <h4>Menata Ruang Keluarga</h4>
+      <hr class="style-one"> </hr>
+      <p class="blog-text">Masukkan penjelasan singkat disini atau beberapa kata dan kalimat pertama dari blog yang dituju.</p>
+    </div>
+
+  </div>
 </div>
+
+
+</div>
+
 
 <!-- ADS CONTENT -->
 <div class="col-md-3">.col-md-1</div>
--- application/views/slider.php
+++ application/views/slider.php
@@ -1,32 +1,36 @@
-<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-  <!-- Indicators -->
-  <ol class="carousel-indicators">
-    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-  </ol>
+<div class="row">
+  <div class="col-md-12">
+    <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+      <!-- Indicators -->
+      <ol class="carousel-indicators">
+        <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+        <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+        <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+      </ol>
 
-  <!-- Wrapper for slides -->
-  <div class="carousel-inner">
-    <div class="item active">
-      <img src="<?php echo IMAGES_URL?>slideshow_1.png" alt="first slide show">
-      <div class="carousel-caption">
-        Masukan Judul disini
+      <!-- Wrapper for slides -->
+      <div class="carousel-inner">
+        <div class="item active">
+          <img src="<?php echo IMAGES_URL?>slideshow_1.png" alt="first slide show">
+          <div class="carousel-caption">
+            Masukan Judul disini
+          </div>
+        </div>
+        <div class="item">
+          <img src="<?php echo IMAGES_URL?>slideshow_2.png" alt="second slide show">
+          <div class="carousel-caption">
+            Masukan Judul disini
+          </div>
+        </div>
       </div>
-    </div>
-    <div class="item">
-      <img src="<?php echo IMAGES_URL?>slideshow_2.png" alt="second slide show">
-      <div class="carousel-caption">
-        Masukan Judul disini
-      </div>
+
+      <!-- Controls -->
+      <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left"></span>
+      </a>
+      <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right"></span>
+      </a>
     </div>
   </div>
-
-  <!-- Controls -->
-  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-    <span class="glyphicon glyphicon-chevron-left"></span>
-  </a>
-  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-    <span class="glyphicon glyphicon-chevron-right"></span>
-  </a>
 </div>
