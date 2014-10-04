687c240617f4b91723d1af3d84c37f3668bfbba3
shanmarkus <shanmarkus@gmail.com>
2014-10-04 13:28:16 +0700

    Fixing Blog Detail

--- application/views/blog_detail.php
+++ application/views/blog_detail.php
@@ -1,3 +1,4 @@
+<!-- Page Header Div -->
 <div class="row">
   <h2 class = "header-blog-detail">BLOG 1</h2>
   <p class = "header-portfolio-text">Pada bagian ini, mohon berikan deskripsi dan penjelasan singkat mengenai blog ini.</p>
@@ -7,13 +8,16 @@
   <div class="col-md-1">
     <img src="<?php echo IMAGES_URL?>image_7.png" class="img-responsive" alt="Responsive image"></img>
   </div>
+  <!-- END Page Header Div -->
 
+  <!-- Blogger Description Div -->
   <div class="col-md-6">
     <p><b>Herlinda A.</b></p>
     <p>Berikan penjelasan singkat mengenai kontributor ini. Baca lagi...</p>
   </div>
 </div>
 
+<!-- Portfolio Details Div -->
 <div class="row pad-top-20">
   <div class="col-md-8">
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupid- atat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
@@ -29,20 +33,25 @@
          <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam volupta- tem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>
        </div>
      </div>
+     <!-- END Portfolio Details Div -->
    </div>
+   <!-- END Blogger Description Div -->
 
+   <!-- Comment Content Div -->
    <div class="row pad-top-20">
     <div class="col-md-12">
      <div class="komentar_box">
       <h2>KOMENTAR</h2>
       <form role="form">
        <div class="form-group">
-        <label for="name">Name</label>
+        <label for="name">Name
+        </label>
         <input type="text" class="form-control" id="name" 
         placeholder="Enter Name">
       </div>
       <div class="form-group">
-        <label for="name">Komentar</label>
+        <label for="name">Komentar
+        </label>
         <input type="text" class="form-control" id="name" 
         placeholder="Enter Komentar">
       </div>
@@ -51,7 +60,9 @@
   </div>
 </div>
 </div>
+<!-- END Comment Content Div -->
 
+<!-- Lihat Komentar Lain Div -->
 <div class="row pad-top-20">
   <div class ="col-md-12">
     <div class="blog-load-more">
@@ -63,11 +74,14 @@
     </div>
   </div>
 </div>
+<!-- Lihat Komentar Lain Div -->
 </div>
 </div>
 
-<!-- BLOG CONTENT -->
+<!-- Right sidebar CONTENT -->
 <div class="col-md-3 col-md-offset-1">
+
+  <!-- Blog CONTENT -->
   <div class="row blog-detail ">
     <div class="col-md-12">
       <h4>Blog</h4>
@@ -120,12 +134,16 @@
  </div>
 </div> 
 </div>
+<!-- END Blog CONTENT -->
 
+<!-- Advertisement CONTENT -->
 <div class="row pad-top-20">
   <div class="col-md-12">
     <img src="<?php echo IMAGES_URL?>akina.png" class="img-responsive" alt="Responsive image"></img>
   </div>
 </div>
-</div>
+<!-- END Advertisement CONTENT -->
 
+</div>
+<!--END Right sidebar CONTENT -->
 
